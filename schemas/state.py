7e3b65c2ca81c951e15ea10e9b20b7d2fe1ddbeb
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-01-08 13:27:13 +0500

    fafaf

--- schemas/state.py
+++ schemas/state.py
@@ -7,11 +7,4 @@ from db import get_db
 class NewState(BaseModel):
     name: str
     region_id: int
-    @validator('name')
-    def unique_name(self, cls: BaseModel, v: str) -> str:
-        db = next(get_db())
-        users = db.query(State).filter_by(region_id=self.region_id).all()
-        db.commit()
-        if str(v) in set(map(lambda u: u.name, users)):
-            raise HTTPException(status_code=400, detail='Nom raqam allaqachon foydalanilgan')
-        return v
+   
